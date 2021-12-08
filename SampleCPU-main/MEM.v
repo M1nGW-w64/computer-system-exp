941f1682354aca0485ef6c8c107ea8c7f7c6fa7b
eva10084 <128120727@qq.com>
2021-12-08 20:05:51 +0800

    Revert "2021年12月8号，邱聖邦添加了lb指令"
    This reverts commit 8f462aa69190d7dee93296baf1679baf9c830fce.

--- SampleCPU-main/MEM.v
+++ SampleCPU-main/MEM.v
@@ -5,7 +5,7 @@ module MEM(
     // input wire flush,
     input wire [`StallBus-1:0] stall,
 
-    input wire [`EX_TO_MEM_WD-1+64+1+2+4:0] ex_to_mem_bus,
+    input wire [`EX_TO_MEM_WD-1+64+1+2:0] ex_to_mem_bus,
     input wire [31:0] data_sram_rdata,
 
     output wire [`MEM_TO_WB_WD-1+64+1+2:0] mem_to_wb_bus,
@@ -14,7 +14,7 @@ module MEM(
     output wire [37+64+1+2:0] mem_to_id_forwarding
 );
 
-    reg [`EX_TO_MEM_WD-1+64+1+2+4:0] ex_to_mem_bus_r;
+    reg [`EX_TO_MEM_WD-1+64+1+2:0] ex_to_mem_bus_r;
 
     always @ (posedge clk) begin
         if (rst) begin
@@ -44,9 +44,7 @@ module MEM(
     wire inst_div_or_divu_or_mul;
     wire lo_wen;
     wire hi_wen;
-    wire[3:0] load_select;
     assign {
-    load_select,
      lo_wen,
          hi_wen,
     inst_div_or_divu_or_mul,
@@ -59,18 +57,10 @@ module MEM(
         rf_waddr,       // 36:32
         ex_result       // 31:0
     } =  ex_to_mem_bus_r;
-    wire [31:0]load_deal;
-    assign load_deal= (load_select==4'b0000)?data_sram_rdata:
-                       (load_select==4'b1001&&ex_result[1:0]==2'b00)?{{24{data_sram_rdata[7]}},data_sram_rdata[7:0]}:
-                       (load_select==4'b1001&&ex_result[1:0]==2'b01)?{{24{data_sram_rdata[15]}},data_sram_rdata[15:8]}:
-                       (load_select==4'b1001&&ex_result[1:0]==2'b10)?{{24{data_sram_rdata[23]}},data_sram_rdata[23:16]}:
-                       (load_select==4'b1001&&ex_result[1:0]==2'b11)?{{24{data_sram_rdata[31]}},data_sram_rdata[31:24]}:
-                       (load_select==4'b0001)?{24'b0,data_sram_rdata[7:0]}:
-                       (load_select==4'b0011)?{16'b0,data_sram_rdata[15:0]}:
-                       (load_select==4'b1011)?{{16{data_sram_rdata[15]}},data_sram_rdata[15:0]}:
-                       data_sram_rdata;
 
-    assign rf_wdata = (data_ram_wen==4'b0000&&data_ram_en==1'b1)?load_deal:sel_rf_res ? mem_result : ex_result;
+
+
+    assign rf_wdata = (data_ram_wen==4'b0000&&data_ram_en==1'b1)?data_sram_rdata:sel_rf_res ? mem_result : ex_result;
 //    assign rf_wdata = sel_rf_res ? mem_result : ex_result;
     assign mem_to_wb_bus = {
      lo_wen,
